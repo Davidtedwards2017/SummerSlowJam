7cb160e382ec751c1091aa4342597ff6756dcc1b
Cscandariato <cscandariato@gmail.com>
2016-07-14 01:10:42 -0700

    Added better art to most platforms. Created light flicker on all buildings. Fixed some platforms for new art.

---
+++ Death's Head/Assets/Scripts/LightFlicker.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class LightFlicker : MonoBehaviour {
+
+	SpriteRenderer mySprite;
+	float alphaLev;
+
+	void Awake ()
+	{
+		mySprite = this.gameObject.GetComponent<SpriteRenderer> ();
+	}
+
+
+	// Use this for initialization
+	void Start () 
+	{
+		StartCoroutine ("Flicker");
+
+	}
+
+	IEnumerator Flicker ()
+	{
+		while (true) {
+			yield return new WaitForSeconds (.05f);
+			alphaLev = 1.0f * Random.Range (.8f, 1.0f);
+			mySprite.color = new Color (1, 1, 1, alphaLev);
+		} 
+	}
+}
